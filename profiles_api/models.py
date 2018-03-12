4bcb036567b278d71b9c885637ce31fd4f025655
DiegoSGITPolimentes <diego.sanchez@polimentes.com>
2018-03-12 13:38:12 -0600

    feat: migration

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -6,17 +6,18 @@ from django.contrib.auth.models import BaseUserManager
 # Create your models here.
 
 class UserProfileManager(BaseUserManager):
-     """ helds Django work with our custom  user model. """
+    """ helds Django work with our custom  user model. """
 
-     def create_user(self, email, name, password=None):
-         """ Creates a new user profile object. """
-         if not email:
-             raise ValueError('Users must have email address.')
+    def create_user(self, email, name, password=None):
+        """ Creates a new user profile object. """
+        if not email:
+            raise ValueError('Users must have email address.')
 
         email = self.normalize_email(email)
         user = self.model(email=email, name=name)
         user.set_password(password)
         user.save(using=self._db)
+
         return user
 
     def create_superuser(self, email, name, password):
